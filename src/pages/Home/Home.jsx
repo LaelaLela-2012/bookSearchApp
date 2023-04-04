8ece638a7d59db1d79d80a9e8e162c165e475a39
laela-2012 <lela.office123@gmail.com>
2023-04-04 18:03:39 +0700

    Router

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../component/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
