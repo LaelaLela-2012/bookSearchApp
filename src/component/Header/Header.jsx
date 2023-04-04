43d7b772dc83741ed1ffbbbdc948d17a95fd6bbf
laela-2012 <lela.office123@gmail.com>
2023-04-04 18:38:19 +0700

    Navbar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import SearchForm from '../SearchForm/SearchForm';
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        <div className="header-content flex flex-c text-center text-white">
+          <h2 className="header-title text-capitalize">
+            Find Your Book Here!
+          </h2><br />
+          <p className="header-text fs-18 fw-3">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+          <SearchForm />
+        </div>
+      </header>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../component/Header/Header'
+import { Outlet } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <main>
-
+        <Header />
+        <Outlet />
       </main>
     </div>
   )
